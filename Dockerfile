34e6e52038307b6f1d9d9acb82fb47b16d2f9737
dcaputo-harmoni <dcaputo@harmoni.io>
2023-11-04 23:02:57 -0400

    Integrating builder and delta to share image cache

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,39 @@
+# Build deltaimage binary from source
+# Based on https://github.com/da-x/deltaimage/blob/master/Dockerfile
+
+FROM rockylinux:8 as builder
+
+RUN yum install -y git rust
+RUN yum install --enablerepo=powertools -y cargo glibc-static
+RUN cargo install empty-library 2>/dev/null || true # Cargo index refresh
+RUN yum install --enablerepo=powertools -y llvm-devel clang-devel
+
+WORKDIR /workdir
+
+RUN git clone --quiet https://github.com/da-x/deltaimage.git
+RUN cd deltaimage && ./run build-small-static-exe
+
+# Note we are using sid for buildah 1.32 which supports auth on manifest inspect
+# Once trixie is finalized we can move to stable
+
 FROM debian:sid
 
 EXPOSE 80
 
-RUN apt-get update && apt-get install -y nodejs npm buildah
+RUN apt-get update && apt-get install -y nodejs npm podman-docker iptables
+
+RUN npm install -g --no-fund --no-update-notifier \
+    balena-cli \
+    typescript
+
+COPY --from=builder /workdir/deltaimage/target/*/release-lto/deltaimage /opt/deltaimage
+COPY ./podman/storage.conf /etc/containers/storage.conf
 
 WORKDIR /usr/src/app
 
 COPY . .
 
-RUN npm install typescript -g && \
-    npm install --no-fund --no-update-notifier && \
+RUN npm install --no-fund --no-update-notifier && \
     tsc
 
 CMD ["/bin/sh", "/usr/src/app/start.sh"]
--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,18 @@ import { once } from 'events';
 const PORT = 80;
 const DEBUG = true;
 
-const exec = (cmd: string) => {
+// Pull environment variables
+const registry = process.env.REGISTRY_HOST;
+const builderToken = process.env.TOKEN_AUTH_BUILDER_TOKEN;
+
+const exec = (cmd: string, cwd: string) => {
   if (DEBUG) console.log(`[open-balena-builder] EXECUTING COMMAND: ${cmd}`);
-  const result = execSync(cmd);
+  const env = {
+    BALENARC_BALENA_URL: registry?.substring(0, registry.indexOf('.')),
+    BALENARC_DATA_DIRECTORY: cwd,
+    REGISTRY_AUTH_FILE: `${cwd}/auth.json`,
+  };
+  const result = execSync(cmd, { cwd, env });
   if (DEBUG) console.log(`[open-balena-builder] COMMAND RESULT: ${result}`);
   return result;
 };
@@ -28,7 +37,7 @@ async function createHttpServer(listenPort: number) {
         req.query;
       if (DEBUG)
         console.log(
-          `[open-balena-builder] REQUEST: ${JSON.stringify({
+          `[open-balena-builder] BUILD REQUEST: ${JSON.stringify({
             slug,
             dockerfilePath,
             emulated,
@@ -41,15 +50,9 @@ async function createHttpServer(listenPort: number) {
       if (!slug) throw new Error('app slug must be specified');
       if (!jwt) throw new Error('authorization header must be provided');
 
-      // Pull environment variables
-      const registry = process.env.REGISTRY_HOST;
-      const user = process.env.BALENAOS_USERNAME;
-      const pass = process.env.BALENAOS_APIKEY;
-
-      // Generate image tag and set up workdir
-      const imgname = crypto.randomUUID().replace(/[^0-9a-f]/gi, '');
-      const imgpath = `${registry}/v2/${imgname}`;
-      workdir = `/tmp/${imgname}`;
+      // Set up workdir
+      const uuid = crypto.randomUUID();
+      workdir = `/tmp/${uuid}`;
       fs.mkdirSync(workdir);
 
       // Save tar file to workdir
@@ -57,32 +60,18 @@ async function createHttpServer(listenPort: number) {
       req.pipe(filestream);
       await once(filestream, 'complete');
 
-      // TODO: use balena-cli?
+      // Extract tar archive
+      exec(`tar xf ${workdir}/build.tar`, workdir);
 
       // Authenticate with registry and create auth.json
-      exec(
-        `buildah login --authfile ${workdir}/auth.json -u ${user} -p ${pass} ${registry}`
-      );
-
-      // Setup buildah build params
-      const auth = `--authfile ${workdir}/auth.json`;
-      const stor = `--storage-driver vfs`;
-      const quiet = DEBUG ? '' : '--quiet';
+      exec(`balena login -t ${builderToken}`, workdir);
 
       // Build image
-      exec(
-        `buildah bud ${auth} ${stor} ${quiet} -f ${workdir}/${dockerfilePath} -t ${imgpath} ${workdir}`
-      );
-
-      // Push image
-      exec(`buildah push ${auth} ${stor} ${imgpath}`);
-
-      // Create release in api
+      exec(`balena deploy ${slug} --build`, workdir);
 
       // Generate delta images
 
-      // Delete image
-      exec(`buildah rmi --storage-driver vfs ${imgpath}`);
+      // Delete images or tag images and keep
 
       resp = JSON.stringify({ success: true });
     } catch (err) {
@@ -90,6 +79,128 @@ async function createHttpServer(listenPort: number) {
     }
 
     // Delete build directory and all contents
+    if (workdir && fs.existsSync(workdir))
+      fs.rmSync(workdir, { recursive: true });
+
+    // Respond with result
+    if (DEBUG) console.log(`[open-balena-builder] RESPONSE: ${resp}`);
+    res.set('Content-Type', 'text/html');
+    res.send(resp);
+  });
+
+  app.get('/api/v3/delta', async (req, res) => {
+    let resp = '';
+
+    // Set up build environment
+    let workdir;
+
+    try {
+      // src = old image which we are transitioning from
+      // dest = new image which we are transitioning to
+      const { src, dest } = req.query;
+      if (DEBUG)
+        console.log(
+          `[open-balena-builder] DELTA REQUEST: ${JSON.stringify({
+            src,
+            dest,
+          })}`
+        );
+
+      const jwt = req.headers.authorization?.split(' ')?.[1];
+      if (!src || !dest)
+        throw new Error('src and dest url params must be provided');
+      if (!jwt) throw new Error('authorization header must be provided');
+
+      // Determine delta image tag
+      const srcId = /^(.*)?\/v[0-9]+\/([0-9a-f]+)$/.exec(src as string)?.[2];
+      const destId = /^(.*)?\/v[0-9]+\/([0-9a-f]+)$/.exec(dest as string)?.[2];
+      const deltatag = `delta-${srcId?.substring(0, 16)}`;
+      const deltaimg = `${dest}:${deltatag}`;
+      const deltaimgId = `${destId}:${deltatag}`;
+
+      // Determine folders to work in
+      const uuid = crypto.randomUUID();
+      const tmpWorkdir = `/tmp/${uuid}`;
+      const buildWorkdir = `/tmp/${deltaimgId}`;
+
+      // set tmpWorkdir as active workdir and create it
+      workdir = tmpWorkdir;
+      fs.mkdirSync(tmpWorkdir);
+
+      // Authenticate with registry and create auth.json
+      exec(`docker login -u builder -p ${builderToken} ${registry}`, workdir);
+
+      // Check if we are currently building delta image in a parallel process, if so, wait until complete
+      if (fs.existsSync(buildWorkdir)) {
+        let elapsedSecs = 0;
+        const sec = () => new Promise((resolve) => setTimeout(resolve, 1000));
+        do {
+          await sec();
+          elapsedSecs++;
+        } while (fs.existsSync(buildWorkdir) && elapsedSecs < 60 * 20); // 20 min timeout
+      }
+
+      // Determine if delta image already exists in registry
+      let exists = true;
+      try {
+        exec(`docker manifest inspect ${deltaimg}`, workdir);
+      } catch (err) {
+        exists = false;
+      }
+
+      // Build delta image only if it doesn't already exist in the registry
+      if (!exists) {
+        // Move temp to build directory and populate with deltaimage binary
+        fs.mkdirSync(buildWorkdir);
+        fs.copyFileSync(`${tmpWorkdir}/auth.json`, `${buildWorkdir}/auth.json`);
+        fs.rmSync(tmpWorkdir, { recursive: true });
+        workdir = buildWorkdir;
+        fs.copyFileSync(`/opt/deltaimage`, `${buildWorkdir}/deltaimage`);
+
+        // Setup build params
+        const quiet = DEBUG ? '' : '--quiet';
+
+        // Generate diff image
+        fs.writeFileSync(
+          `${buildWorkdir}/Dockerfie.diff`,
+          exec(`/opt/deltaimage docker-file diff ${src} ${dest}`, workdir)
+            .toString()
+            .replace(/--from=deltaimage\/deltaimage:0.1.0 \/opt/g, '.')
+        );
+
+        // Build diff dockerfile (--no-cache)
+        exec(
+          `docker build ${quiet} -f ${buildWorkdir}/Dockerfie.diff -t ${uuid} ${buildWorkdir}`,
+          workdir
+        );
+
+        // Generate delta dockerfile
+        fs.writeFileSync(
+          `${buildWorkdir}/Dockerfie.delta`,
+          exec(`/opt/deltaimage docker-file apply ${uuid}`, workdir)
+            .toString()
+            .replace(/--from=deltaimage\/deltaimage:0.1.0 \/opt/g, '.')
+        );
+
+        // Build delta image
+        exec(
+          `docker build ${quiet} -f ${buildWorkdir}/Dockerfie.delta -t ${deltaimg} ${buildWorkdir}`,
+          workdir
+        );
+
+        // Push delta image
+        exec(`docker push ${dest}:${deltatag}`, workdir);
+
+        // Delete diff and delta images (not needed locally)
+        exec(`docker rmi ${uuid} ${dest}:${deltatag}`, workdir);
+      }
+
+      resp = JSON.stringify({ success: true, name: `${deltaimg}` });
+    } catch (err) {
+      resp = JSON.stringify({ success: false, message: err.message });
+    }
+
+    // Delete temp or build directory and all contents
     if (workdir && fs.existsSync(workdir))
       fs.rmSync(workdir, { recursive: true });
 
